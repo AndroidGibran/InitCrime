914a701b0a62d5e4eca1c277dc5117c7705f8562
AndroidGibran <jibran.s1995@gmail.com>
2015-08-19 22:12:21 +0500

    Crime with API 23

---
+++ Crime/app/src/main/java/com/example/gibraniqbal/crime/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.example.gibraniqbal.crime;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ Crime/app/src/main/java/com/example/gibraniqbal/crime/CrimeListFragment.java
@@ -0,0 +1,133 @@
+package com.example.gibraniqbal.crime;
+
+import java.util.ArrayList;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class CrimeListFragment extends ListFragment {
+    private ArrayList<Crime> mCrimes;
+    private boolean mSubtitleVisible;
+    private ActionBar actionBar;
+//    private CrimeListActivity activity;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        setRetainInstance(true);
+        actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        getActivity().setTitle(R.string.crimes_title);
+        mCrimes = CrimeLab.get(getActivity()).getCrimes();
+        CrimeAdapter adapter = new CrimeAdapter(mCrimes);
+        setListAdapter(adapter);
+        setRetainInstance(true);
+        mSubtitleVisible = false;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, parent, savedInstanceState);
+        
+//           if (mSubtitleVisible) {
+//                actionBar.setSubtitle(R.string.subtitle);
+//            }
+        
+        return v;
+    }
+
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // get the Crime from the adapter
+        Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
+        // start an instance of CrimePagerActivity
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
+        startActivityForResult(i, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
+        MenuItem showSubtitle = menu.findItem(R.id.menu_item_show_subtitle);
+//        if (mSubtitleVisible && showSubtitle != null) {
+//            showSubtitle.setTitle(R.string.hide_subtitle);
+//        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent i = new Intent(getActivity(), CrimeActivity.class);
+                i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+                startActivityForResult(i, 0);
+                return true;
+            case R.id.menu_item_show_subtitle:
+            	if (actionBar.getSubtitle() == null) {
+                    actionBar.setSubtitle(R.string.subtitle);
+                    mSubtitleVisible = true;
+//                    item.setTitle(R.string.hide_subtitle);
+            	}  else {
+            		actionBar.setSubtitle(null);
+            		 mSubtitleVisible = false;
+//            		item.setTitle(R.string.show_subtitle);
+            	}
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        } 
+    }
+
+    private class CrimeAdapter extends ArrayAdapter<Crime> {
+        public CrimeAdapter(ArrayList<Crime> crimes) {
+            super(getActivity(), android.R.layout.simple_list_item_1, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // if we weren't given a view, inflate one
+            if (null == convertView) {
+                convertView = getActivity().getLayoutInflater()
+                    .inflate(R.layout.list_item_crime, null);
+            }
+
+            // configure the view for this Crime
+            Crime c = getItem(position);
+
+            TextView titleTextView =
+                (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+            TextView dateTextView =
+                (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+            CheckBox solvedCheckBox =
+                (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
+        }
+    }
+
+}
+
